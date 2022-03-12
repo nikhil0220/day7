ad2ab10e25f04b827e5f6f3480af7f3de69e43d5
Bonala <2085840@cognizant.com>
2022-03-12 13:55:40 +0530

    modified

---
+++ WebApplicationDay7/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationDay7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string display()
+        {
+            return ("Display method in vlaues controller");
+        }
+    }
+}
